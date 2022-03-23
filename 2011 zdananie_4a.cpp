1126ab036ad60a71930e2a89c8502185e1304fce
slo-1c-15-2021 <81419326+slo-1c-15-2021@users.noreply.github.com>
2022-03-23 08:42:13 +0100

    Add files via upload

---
+++ 2011 zdananie_4a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>>
+using namespace std;
+bool czy_palindrom(string s)
+{
+    int p, k;
+    p=0;
+    k=s.size()-1;
+    while(p<k){
+        if(s[p] !=s[k]) return false;
+        p++;
+        k--;
+
+    }
+    return true;
+}
+int main(){
+    string s;
+        int licznik=0;
+    ifstream f("hasla.txt");
+    while(f>>s) {
+       if(s.size()%2== 0) licznik++;
+       while (f>>s) {
+            if(czy_palindrom(s)) licznik ++;
+            }
+    }
+    cout <<"odp. b =" << licznik << "\n";
+}
